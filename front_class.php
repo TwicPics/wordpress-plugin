2492f68939074f8e71ee4c06d24e1489f547b3c9
nicolascassette <nicolas@studiocassette.com>
2020-04-28 14:44:07 +0200

    Commented fix for img tag under bg

--- front_class.php
+++ front_class.php
@@ -286,20 +286,20 @@ class TwicPics {
         default : $new_style_attr.= $property.':'.$value.';';
       }
     }
-    
+
     if( isset($bg_urls) && is_array($bg_urls) ){
       $tag->setAttribute('style',$new_style_attr);
       $tag->setAttribute('class', $tag->getAttribute( 'class' ) . " twic" );
       $tag->setAttribute('data-background', 'url('.$bg_urls[0].')' );
 
       if ( isset( $x ) && isset( $y ) ) {
-        if ( $x != 50 || $y != 50 ) 
+        if ( $x != 50 || $y != 50 )
           $tag->setAttribute('data-background-transform', "focus={$x}px{$y}p/auto");
       }
-      
-      foreach ( $tag->getElementsByTagName( 'img' ) as $img ) {
-        $tag->removeChild( $img );
-      }
+
+      // foreach ( $tag->getElementsByTagName( 'img' ) as $img ) {
+      //   $tag->removeChild( $img );
+      // }
     }
   }
 
