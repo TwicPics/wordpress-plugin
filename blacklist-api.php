ceb9031da390bbbf68364a94e0811e5febaf6399
Eloïse <eloise-martin@live.fr>
2022-12-22 12:19:47 +0100

    Adds an 'optimization level' parameter. This allows users to choose whether images are statically or dynamically  optimized.

---
+++ blacklist-api.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Blacklisted plugins (in which images shouldn't be optimized by TwicPics)
+ *
+ * @package TwicPics
+ */
+
+$plugins_blacklist = array(
+	'Smart Slider Plugin' => 'wp-block-nextend-smartslider3',
+	'Slider'              => 'wds_slider_cont',
+);
