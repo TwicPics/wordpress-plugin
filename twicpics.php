1ccefa55c163c40e9a9ac4eed2f5ea15aaaf9abc
Eloïse <eloise-martin@live.fr>
2023-01-31 18:43:37 +0100

    Updates plugin version

--- twicpics.php
+++ twicpics.php
@@ -4,14 +4,14 @@
  *
  * @package          TwicPics
  * @link             https://www.twicpics.com/
- * @version          0.2.0
+ * @version          0.2.1
  * @license          gpl-2.0
  *
  * @wordpress-plugin
  * Plugin Name:      TwicPics
  * Plugin URI:       https://www.twicpics.com/documentation/
  * Description:      Delivers pixel perfect images on the fly.
- * Version:          0.2.0
+ * Version:          0.2.1
  * License:          GPL-2.0+
  * Licence URI:      http://www.gnu.org/licenses/gpl-2.0.txt
  * Author:           TwicPics
